bcf9342b5c2d90902ad625d1bb501d1d5afd287b
HeikkiTakalo <heikkitakalo@windowslive.com>
2024-12-18 09:52:09 +0200

    Database connection and tweaks to server.js

--- server.js
+++ server.js
@@ -1,58 +1,43 @@
 import express from 'express';
+import pkg from 'pg';
+const { Client } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
-app.listen(3001, () => {
-    console.log('Server running in port 3001');
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-app.post('/genre', (req,res) => {
-    const { name } = req.body;
-    console.log({ name });
-})
-app.post('/movie', (req, res) => {
-    const { name, year, genre } = req.body;
-    console.log({ name, year, genre });
+
+client.connect()
+  .then(() => console.log('Connection to postrgesql was succesful!'))
+  .catch((err) => console.error('Error in postgresql connection!:', err.stack));
+
+
+app.get('/Genre', async (req, res) => {
+  try {
+    const result = await client.query('SELECT * FROM Genre');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error');
+  }
 });
 
-app.post('/register', (req, res) => {
-    const { name, username, password, yearOfBirth } = req.body;
-    console.log({ name, username, password, yearOfBirth });
-});
+app.get('/Movie', async (req, res) => {
+    try {
+      const result = await client.query('SELECT * FROM Movie');
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error');
+    }
+  });
 
-app.get('/movie/:id', (req, res) => {
-    const { id } = req.params;
-    console.log({ id });
-});
-
-app.delete('/movie/:id', (req, res) => {
-    const { id } = req.params;
-    console.log({ id });
-});
-
-app.get('/movies', (req, res) => {
-    const { page } = req.query;
-    console.log({ page });
-});
-
-app.get('/movies/search', (req, res) => {
-    const { keyword } = req.query;
-    console.log({ keyword });
-});
-
-app.post('/review', (req, res) => {
-    const { username, stars, reviewText, movieId } = req.body;
-    console.log({ username, stars, reviewText, movieId });
-});
-
-app.post('/favorites', (req, res) => {
-    const { username, movieId } = req.body;
-    console.log({ username, movieId });
-});
-
-app.get('/favorites/:username', (req, res) => {
-    const { username } = req.params;
-    console.log({ username });
-});
